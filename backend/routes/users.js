551cb04a2d13baf94e7a8daed522aa9e8223712b
RalfiSlask <nilssonnarki@gmail.com>
2024-02-01 09:26:50 +0100

    feat: finalize post for adding user to database with security measures and uuid

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CryptoJS = require('crypto-js');
+const { v4: uuidv4 } = require('uuid');
 const dotenv = require('dotenv');
 const {
   getEncryptedData,
@@ -31,8 +31,47 @@ router.post('/', (req, res) => {
   // Get one type of user, send whole object
 });
 
+/**
+ * Adds user with encrypted password and id as uuid
+ * Checks if user email already exists, if it does exists method
+ */
 router.post('/add', (req, res) => {
-  // Create user
+  const { userName, email, password } = req.body;
+  req.app.locals.db
+    .collection('users')
+    .findOne({ email: email })
+    .then((emailMatches) => {
+      if (emailMatches) {
+        console.log('email: ', email);
+        res.status(403).json({ err: 'user already exist' });
+        return;
+      }
+      const encryptedPassword = getEncryptedData(
+        password,
+        process.env.SALT_KEY
+      );
+      console.log(encryptedPassword);
+      req.app.locals.db
+        .collection('users')
+        .insertOne({
+          id: uuidv4(),
+          userName: userName,
+          email: email,
+          password: encryptedPassword,
+        })
+        .then((insertResult) => {
+          if (insertResult.acknowledged) {
+            console.log('sent user', req.body);
+            res.status(201).json({ user: userName, email: email });
+          } else {
+            res.status(500).json({ err: 'could not add user' });
+          }
+        })
+        .catch((err) => {
+          console.log(err, 'could not add user');
+          res.status(500).json({ err: 'could not add user' });
+        });
+    });
 });
 
 router.post('login', (req, res) => {
