64bac6f4eb649333d4bde7d17eac0f8bd320218e
RalfiSlask <nilssonnarki@gmail.com>
2024-02-01 06:20:37 +0100

    feat: finalize get request for all products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Recieveing all products as array of object
+ */
 router.get('/', function (req, res) {
-  // Get all products
-  res.send('respond with a resource');
+  req.app.locals.db
+    .collection('products')
+    .find()
+    .toArray()
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((err) => {
+      console.error(err, 'could not recieve products');
+      res.status(500).json({ error: 'Could not recieve products' });
+    });
 });
 
+/**
+ * Recieving a specifik product using id parameter
+ */
 router.get('/:id', (req, res) => {
   // With product id get product
 });
 
+/**
+ * For creating and adding product
+ */
 router.post('/add', (req, res) => {
   // Create product
 });
