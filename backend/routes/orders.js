969ce5514e78c5bc45ed4ac0dedeafde321561e0
RalfiSlask <nilssonnarki@gmail.com>
2024-02-01 14:13:53 +0100

    feat: finalize getting orders from db request

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
 router.get('/', function (req, res) {
   res.send('respond with a resource');
 });
@@ -10,8 +9,22 @@ router.post('/add', (req, res) => {
   // Create order for specific user
 });
 
+/**
+ * Receving all the stored orders
+ */
 router.get('/all', (req, res) => {
   // Get all orders
+  req.app.locals.db
+    .collection('orders')
+    .find()
+    .toArray()
+    .then((ordersData) => {
+      res.json({ orders: ordersData });
+    })
+    .catch((err) => {
+      console.log(err, 'could not get all orders');
+      res.status(500).json({ err: 'could not get all orders' });
+    });
 });
 
 module.exports = router;
