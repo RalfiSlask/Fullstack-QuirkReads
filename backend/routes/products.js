195d4dc90015e3cc711bae20b568118f8417a141
RalfiSlask <nilssonnarki@gmail.com>
2024-02-01 06:56:23 +0100

    feat: finalize post request for adding new product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 /**
  * Receiving all products, response as array of objects
@@ -25,7 +26,7 @@ router.get('/:id', (req, res) => {
   console.log(req.params.id);
   req.app.locals.db
     .collection('products')
-    .findOne({ 'id': req.params.id })
+    .findOne({ id: req.params.id })
     .then((product) => {
       if (product) {
         console.log(product);
@@ -44,6 +45,29 @@ router.get('/:id', (req, res) => {
  * For creating and adding product
  */
 router.post('/add', (req, res) => {
+  const { title, author, price, storage } = req.body;
+  req.app.locals.db
+    .collection('products')
+    .insertOne({
+      title: title,
+      author: author,
+      price: price,
+      storage: storage,
+      id: uuidv4(),
+    })
+    .then((insertResult) => {
+      // if insertOne operation goes through
+      if (insertResult.acknowledged) {
+        console.log('sent product:', req.body);
+        res.json({ ...req.body, id: uuidv4() });
+      } else {
+        res.status(500).json({ err: 'could not add product' });
+      }
+    })
+    .catch((err) => {
+      console.log(err, 'could not add product');
+      res.status(500).json({ err: 'could not add product' });
+    });
   // Create product
 });
 
