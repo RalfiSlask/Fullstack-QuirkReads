d07637e5f5e1f88738c6108fbcdb35cc4effbc6a
RalfiSlask <nilssonnarki@gmail.com>
2024-02-01 08:50:26 +0100

    feat: add utils folder with helperfunctions file, add and export functions for encrypting and decrypting data

---
+++ backend/utils/helperfunctions.js
@@ -0,0 +1,15 @@
+const getEncryptedData = (data, saltKey) => {
+  return CryptoJS.AES.encrypt(data, saltKey).toString();
+};
+
+const getDecryptedData = (encryptedData, saltKey) => {
+  return (decryptedPassword = CryptoJS.AES.decrypt(
+    encryptedData,
+    saltKey
+  ).toString(CryptoJS.enc.Utf8));
+};
+
+module.exports = {
+  getDecryptedData,
+  getEncryptedData,
+};
