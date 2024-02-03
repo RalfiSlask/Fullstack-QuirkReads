21c8a6b29e541264c9260bf61b04bfc633f61d53
RalfiSlask <nilssonnarki@gmail.com>
2024-02-03 08:54:00 +0100

    docs: add custom colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F2F2F2',
+        'secondary': '#FFFFFF',
+        'primaryBtn': '#B0C4DE',
+        'primaryBtn-hover': '#A9A9A9',
+        'primaryBtn-shadow': 'rgba(0, 0, 0, 0.1)',
+        'header': '#B0C4DE',
+      },
+    },
   },
   plugins: [],
 };
