f8a4370075fbb24c6411b5d41f393ea9eb80315c
RalfiSlask <nilssonnarki@gmail.com>
2024-02-03 09:03:22 +0100

    feat: import login and library contexts in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.scss';
+import { LoginContextProvider } from './context/LoginContext.tsx';
+import { LibraryContextProvider } from './context/LibraryContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LoginContextProvider>
+      <LibraryContextProvider>
+        <App />
+      </LibraryContextProvider>
+    </LoginContextProvider>
   </React.StrictMode>
 );
