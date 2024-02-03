2b2f62a332add99d0ba11153db7a664a863efe81
RalfiSlask <nilssonnarki@gmail.com>
2024-02-03 13:43:56 +0100

    feat: add create account screen and library screen components as routes in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginScreen from './components/pages/LoginScreen.tsx';
+import LoginScreen from './components/pages/login/LoginScreen.tsx';
 import './styles/index.scss';
 import { LoginContextProvider } from './context/LoginContext.tsx';
 import { LibraryContextProvider } from './context/LibraryContext.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CreateAccountScreen from './components/pages/createaccount/CreateAccountScreen.tsx';
+import LibraryScreen from './components/pages/library/LibraryScreen.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,6 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginScreen />} />
+            <Route path="/create" element={<CreateAccountScreen />} />
+            <Route path="/library" element={<LibraryScreen />} />
           </Routes>
         </BrowserRouter>
       </LibraryContextProvider>
