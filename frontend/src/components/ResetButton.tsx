c0fc09eb84ff94ef15f5e9009c4b8376a181549a
RalfiSlask <nilssonnarki@gmail.com>
2024-02-03 10:47:17 +0100

    feat: add resetbutton and submit button components

---
+++ frontend/src/components/ResetButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResetButton = () => {
+  return (
+    <button type="reset" className="button-primary">
+      Reset
+    </button>
+  );
+};
+
+export default ResetButton;
