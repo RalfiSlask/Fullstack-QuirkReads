775b451320095fe7373eab5515d411e540693625
RalfiSlask <nilssonnarki@gmail.com>
2024-02-03 10:46:10 +0100

    feat: add browser router and create path for LoginScreen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import LoginScreen from './components/pages/LoginScreen.tsx';
 import './styles/index.scss';
 import { LoginContextProvider } from './context/LoginContext.tsx';
 import { LibraryContextProvider } from './context/LibraryContext.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LoginContextProvider>
       <LibraryContextProvider>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginScreen />} />
+          </Routes>
+        </BrowserRouter>
       </LibraryContextProvider>
     </LoginContextProvider>
   </React.StrictMode>
