c0bb156ac92a934c8930fbde6fd221055e4aadc7
RalfiSlask <nilssonnarki@gmail.com>
2024-02-04 11:48:21 +0100

    feat: added a create account screen with goback button and the create account form

--- frontend/src/components/pages/createaccount/CreateAccountScreen.tsx
+++ frontend/src/components/pages/createaccount/CreateAccountScreen.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import CreateAccountForm from './CreateAccountForm';
+import GoBackButton from './GoBackButton';
 
 const CreateAccountScreen = () => {
-  return <div>CreateAccountScreen</div>;
+  return (
+    <main className="w-full flex justify-center relative">
+      <div className="absolute left-4 top-4">
+        <GoBackButton />
+      </div>
+      <div className="flex flex-col gap-10 mt-[200px] w-[600px] items-center">
+        <h1 className="text-[#525252] text-4xl font-bold">Create Account</h1>
+        <CreateAccountForm />
+      </div>
+    </main>
+  );
 };
 
 export default CreateAccountScreen;
