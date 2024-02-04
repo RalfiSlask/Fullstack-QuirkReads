0676151414a8047f7d1a483a42db219d964cb9fd
RalfiSlask <nilssonnarki@gmail.com>
2024-02-04 11:45:35 +0100

    feat: add title prop as string to submit button

--- frontend/src/components/shared/SubmitButton.tsx
+++ frontend/src/components/shared/SubmitButton.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const SubmitButton = () => {
+type SubmitPropsType = {
+  title: string;
+};
+
+const SubmitButton: React.FC<SubmitPropsType> = ({ title }) => {
   return (
     <button type="submit" className="button-primary">
-      Login
+      {title}
     </button>
   );
 };
