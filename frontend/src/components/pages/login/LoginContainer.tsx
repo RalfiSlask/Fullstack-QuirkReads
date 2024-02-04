9424eca3b2fd7201a31f417848dffe2befc0f0aa
RalfiSlask <nilssonnarki@gmail.com>
2024-02-04 11:52:26 +0100

    feat: add handle click on create account function and also imported handle login reset function from the login context

--- frontend/src/components/pages/login/LoginContainer.tsx
+++ frontend/src/components/pages/login/LoginContainer.tsx
@@ -1,13 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
+import { useContext } from 'react';
+import { LoginContext } from '../../../context/LoginContext';
 
 const LoginContainer = () => {
+  const loginContext = useContext(LoginContext);
+  const navigate = useNavigate();
+
+  if (!loginContext) {
+    return;
+  }
+
+  const { handleLoginReset } = loginContext;
+
+  const handleCreateAnAccountOnClick = () => {
+    navigate('/create');
+    handleLoginReset();
+  };
+
   return (
     <section className="w-[700px] flex flex-col gap-10 items-center rounded-xl py-10 bg-secondary shadow-gray-800 mt-[200px]">
       <h1 className="text-3xl">Login</h1>
       <LoginForm />
       <div className="flex items-center gap-4">
         <p className="text-2xl text-grayText">Not Registered Yet?</p>
-        <p className="text-2xl cursor-pointer">Create an account</p>
+        <p onClick={handleCreateAnAccountOnClick} className="text-2xl cursor-pointer">
+          Create an account
+        </p>
       </div>
     </section>
   );
