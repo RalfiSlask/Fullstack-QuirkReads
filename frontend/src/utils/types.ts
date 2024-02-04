79a2453a36fff4448766efc5de7d5af56f38b543
RalfiSlask <nilssonnarki@gmail.com>
2024-02-04 11:53:03 +0100

    feat: added types with interfaces for the input values, both for the login inputs and the create account inputs

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,10 @@
+export interface ICreateAccountFormInputValues {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface ILoginFormInputValues {
+  email: string;
+  password: string;
+}
