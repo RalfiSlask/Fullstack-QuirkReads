3c67f756d30ea269bdf9794f78d55536fbd91312
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 14:30:29 +0100

    feat: add interfaces for Book, Category, Order, Product, Modals in types file

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -8,3 +8,34 @@ export interface ILoginFormInputValues {
   email: string;
   password: string;
 }
+
+export interface IBookType {
+  name: string;
+  description: string;
+  price: number;
+  lager: number;
+  id: string;
+  category: string;
+}
+
+export interface ICategoryType {
+  name: string;
+  id: string;
+}
+
+export interface IOrderType {
+  user: string;
+  products: IProductType[];
+}
+
+export interface IProductType {
+  productId: string;
+  quantity: number;
+}
+
+export interface IModalType {
+  lightbox: boolean;
+  login: boolean;
+  create: boolean;
+  order: boolean;
+}
