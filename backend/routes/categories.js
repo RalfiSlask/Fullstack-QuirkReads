68251c44f317791b5c758e4ed276cd354dac27da
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 14:47:16 +0100

    refactor: refactor categories so it uses method chaining for better visibility and cleaner functions

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -32,36 +32,36 @@ router.get('/add', function (req, res) {
  * Checks if category already exists
  */
 router.post('/add', (req, res) => {
+  const db = req.app.locals.db;
   if (req.body.token !== process.env.TOKEN) {
     console.log('not authorized');
     res.status(401).json({ message: 'not authorized' });
     return;
   }
 
-  req.app.locals.db
-    .collection('categories')
+  db.collection('categories')
     .findOne({ name: req.body.name })
     .then((category) => {
       if (category) {
         console.log(category);
         return res.status(500).json({ err: 'category already exists' });
       }
-      req.app.locals.db
+      return db
         .collection('categories')
-        .insertOne({ name: req.body.name, id: uuidv4() })
-        .then((result) => {
-          if (result.acknowledged) {
-            console.log('added category');
-            res.status(201).json({ created: req.body.name });
-          } else {
-            console.log('category not added');
-            res.status(500).json({ err: 'category not added' });
-          }
-        })
-        .catch((err) => {
-          console.log(err, 'could not add category');
-          res.status(500).json({ err: 'could not add category' });
-        });
+        .insertOne({ name: req.body.name, id: uuidv4() });
+    })
+    .then((result) => {
+      if (result.acknowledged) {
+        console.log('added category');
+        res.status(201).json({ created: req.body.name });
+      } else {
+        console.log('category not added');
+        res.status(500).json({ err: 'category not added' });
+      }
+    })
+    .catch((err) => {
+      console.log(err, 'could not add category');
+      res.status(500).json({ err: 'could not add category' });
     });
 });
 
