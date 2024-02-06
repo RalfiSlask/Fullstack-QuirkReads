bae1e2231fd690c165b03755136df64607316dd4
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 06:25:38 +0100

    feat: add so that we can open the login modal on click, also the logout button is only visible if a user is logged in

--- frontend/src/components/pages/library/Sidebar.tsx
+++ frontend/src/components/pages/library/Sidebar.tsx
@@ -1,14 +1,32 @@
+import { useContext } from 'react';
 import LogoutButton from './LogoutButton';
+import UserComponent from './UserComponent';
+import { LoginContext } from '../../../context/LoginContext';
 
 const Sidebar = () => {
+  const loginContext = useContext(LoginContext);
+
+  if (!loginContext) {
+    return;
+  }
+
+  const { userName, changeStateOfModal } = loginContext;
+
+  const openOrderModalOnClick = () => {
+    changeStateOfModal('lightbox', true);
+    changeStateOfModal('order', true);
+  };
+
   return (
-    <section className="fixed left-0 w-[350px] bg-secondary h-full pt-24 pb-20 pl-28 flex flex-col gap-20">
-      <h1 className="font-bold text-3xl">Library</h1>
+    <section className="fixed left-0 w-[350px] bg-secondary h-full pt-24 pb-20 pl-28 flex flex-col gap-20 border-r border-solid border-primaryBtn">
+      <UserComponent />
       <div className="flex flex-col justify-between h-full w-full">
-        <ul className="flex flex-col gap-2 text-xl">
-          <li>Home</li>
-          <li>Search</li>
-          <li>My Orders</li>
+        <ul className="flex flex-col gap-2 text-xl ">
+          <li className="hover:text-primaryBtn cursor-pointer">Home</li>
+          <li className="hover:text-primaryBtn cursor-pointer">Search</li>
+          <li onClick={openOrderModalOnClick} className="hover:text-primaryBtn cursor-pointer">
+            My Orders
+          </li>
         </ul>
         <div className="flex flex-col gap-8 items-start">
           <ul className="flex flex-col gap-2 text-lg">
@@ -16,7 +34,7 @@ const Sidebar = () => {
             <li>Support</li>
             <li>Terms & Condition</li>
           </ul>
-          <LogoutButton />
+          {userName.length > 0 && <LogoutButton />}
         </div>
       </div>
     </section>
