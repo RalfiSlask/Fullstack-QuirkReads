bbba3657f619d3d2693fb68dd0b6b7db557aa700
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 06:31:16 +0100

    feat: add userOrder, userName, userName states, functions for changing if modals are open or closed, adding post function for getting a users orders

--- frontend/src/context/LoginContext.tsx
+++ frontend/src/context/LoginContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, FormEvent, ReactNode, useState } from 'react';
-import { ILoginFormInputValues, ICreateAccountFormInputValues } from '../utils/types';
+import { createContext, FormEvent, ReactNode, useState, useEffect, useCallback } from 'react';
+import { ILoginFormInputValues, ICreateAccountFormInputValues, IModalType, IOrderType } from '../utils/types';
 
 export const LoginContext = createContext<undefined | ILoginTypes>(undefined);
 
@@ -9,9 +9,14 @@ interface ILoginTypes {
   createAccountInputValues: ICreateAccountFormInputValues;
   loginErrorMessage: string;
   createAccountErrorMessage: string;
+  loginModals: IModalType;
+  userName: string;
+  userId: string;
+  userOrder: IOrderType;
   // setters
   setLoginErrorMessage: React.Dispatch<React.SetStateAction<string>>;
   setCreateAccountErrorMessage: React.Dispatch<React.SetStateAction<string>>;
+  setUserName: React.Dispatch<React.SetStateAction<string>>;
   // functions
   handleLoginReset: () => void;
   handleCreateAccountReset: () => void;
@@ -20,6 +25,9 @@ interface ILoginTypes {
     inputKey: keyof ICreateAccountFormInputValues,
     e: FormEvent<HTMLInputElement>
   ) => void;
+  changeStateOfModal: (key: keyof IModalType, state: boolean) => void;
+  handleCreateAnAccountOnClick: () => void;
+  closeModalOnClick: () => void;
 }
 
 interface ILoginType {
@@ -31,10 +39,88 @@ export const LoginContextProvider: React.FC<ILoginType> = ({ children }) => {
   const [createAccountInputValues, setCreateAccountInputValues] = useState({ name: '', email: '', password: '' });
   const [loginErrorMessage, setLoginErrorMessage] = useState('');
   const [createAccountErrorMessage, setCreateAccountErrorMessage] = useState('');
+  const [loginModals, setLoginModals] = useState<IModalType>({
+    lightbox: false,
+    login: false,
+    create: false,
+    order: false,
+  });
+  const [userName, setUserName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userOrder, setUserOrder] = useState<IOrderType>({ user: '', products: [] });
+
+  const token = import.meta.env.VITE_TOKEN;
+
+  const addProductToOrder = () => {
+    setUserOrder(prev => ({ ...prev, products: [...prev.products, { productId: '1', quantity: 2 }] }));
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUserName(JSON.parse(storedUser).name);
+      setUserId(JSON.parse(storedUser).id);
+    }
+  }, [userName]);
+
+  useEffect(() => {
+    console.log('userId', userId);
+    console.log('order:', userOrder);
+  }, [userId, userOrder]);
+
+  const fetchOrders = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/orders/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: userId,
+          token: token,
+        }),
+      });
+      if (response.ok) {
+        const jsonData = await response.json();
+        setUserOrder(jsonData);
+      }
+    } catch (err) {
+      console.log(err, 'could not fetch orders');
+    }
+  }, [userId, token]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (userId !== '') {
+          await fetchOrders();
+        }
+      } catch (err) {
+        console.log(err, 'error fetching data');
+      }
+    };
+    fetchData();
+  }, [userId, fetchOrders]);
 
   const handleLoginReset = () => {
     setLoginInputValues({ email: '', password: '' });
     setLoginErrorMessage('');
+  };
+
+  const changeStateOfModal = (key: keyof IModalType, state: boolean) => {
+    setLoginModals(prev => ({ ...prev, [key]: state }));
+  };
+
+  const handleCreateAnAccountOnClick = () => {
+    handleLoginReset();
+    changeStateOfModal('create', true);
+    changeStateOfModal('login', false);
+  };
+
+  const closeModalOnClick = () => {
+    handleLoginReset();
+    changeStateOfModal('login', false);
+    changeStateOfModal('lightbox', false);
   };
 
   const handleCreateAccountReset = () => {
@@ -63,7 +149,12 @@ export const LoginContextProvider: React.FC<ILoginType> = ({ children }) => {
     createAccountInputValues: createAccountInputValues,
     loginErrorMessage: loginErrorMessage,
     createAccountErrorMessage: createAccountErrorMessage,
+    loginModals: loginModals,
+    userName: userName,
+    userId: userId,
+    userOrder: userOrder,
     // setters
+    setUserName: setUserName,
     setLoginInputValues: setLoginInputValues,
     setLoginErrorMessage: setLoginErrorMessage,
     setCreateAccountErrorMessage: setCreateAccountErrorMessage,
@@ -72,6 +163,9 @@ export const LoginContextProvider: React.FC<ILoginType> = ({ children }) => {
     handleCreateAccountReset: handleCreateAccountReset,
     handleLoginInputOnChange: handleLoginInputOnChange,
     handleCreateAccountInputOnChange: handleCreateAccountInputOnChange,
+    changeStateOfModal: changeStateOfModal,
+    handleCreateAnAccountOnClick: handleCreateAnAccountOnClick,
+    closeModalOnClick: closeModalOnClick,
   };
 
   return <LoginContext.Provider value={contextValues}>{children}</LoginContext.Provider>;
