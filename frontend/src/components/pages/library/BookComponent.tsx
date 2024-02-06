6c5a68f46e641de8981faa9095d501f56c45ffc7
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 14:24:53 +0100

    feat: add functions to increment or decrement product quantatites, have logic and styling for when the products storage is zero, paste addProductToCart function to Buy button

--- frontend/src/components/pages/library/BookComponent.tsx
+++ frontend/src/components/pages/library/BookComponent.tsx
@@ -1,12 +1,32 @@
 import bookCover from '../../../assets/images/book-cover.png';
+
+import { LoginContext } from '../../../context/LoginContext';
 import { IBookType } from '../../../utils/types';
+import { useContext, useState } from 'react';
 
 type BookProps = {
   bookInfo: IBookType;
 };
 
 const BookComponent: React.FC<BookProps> = ({ bookInfo }) => {
-  const { name, description, price, lager } = bookInfo;
+  const [bookQuantity, setbookQuantity] = useState(0);
+  const loginContext = useContext(LoginContext);
+
+  if (!loginContext) {
+    return;
+  }
+
+  const { addProductToCart } = loginContext;
+
+  const { name, description, price, id, lager } = bookInfo;
+
+  const decrementBooksOnClick = () => {
+    setbookQuantity((prev: number) => (prev > 0 ? prev - 1 : prev));
+  };
+
+  const incrementBooksOnClick = () => {
+    setbookQuantity(prev => (prev < lager ? prev + 1 : prev));
+  };
 
   return (
     <div className="bg-secondary py-4 px-3 w-[200px] h-[320px] flex flex-col justify-between  items-center rounded-md">
@@ -20,17 +40,27 @@ const BookComponent: React.FC<BookProps> = ({ bookInfo }) => {
         <div className="flex flex-col items-center gap-2">
           <p>${price}</p>
           <div className="flex items-center gap-2">
-            <button>-</button>
-            <div className="px-6 border border-solid border-black py-2 h-[34px] rounded-md">0</div>
-            <button>+</button>
+            <button onClick={decrementBooksOnClick}>-</button>
+            <div className="px-6 border border-solid border-black py-2 h-[34px] rounded-md">{bookQuantity}</div>
+            <button onClick={incrementBooksOnClick}>+</button>
           </div>
         </div>
 
         <div className="flex flex-col items-center justify-between">
           <p>
-            stock: <span className="text-green-600">{lager}</span>
+            {lager > 0 ? 'stock:' : ''}{' '}
+            <span className={`${lager <= 0 ? 'text-red-500' : 'text-green-600'}`}>
+              {lager <= 0 ? 'out of stock' : lager}
+            </span>
           </p>
-          <button className="bg-primaryBtn px-4 rounded-md py-2 hover:bg-primaryBtn-hover h-[34px]">Add</button>
+          <button
+            onClick={() => {
+              addProductToCart(bookQuantity, id);
+            }}
+            className="bg-primaryBtn px-4 rounded-md py-2 hover:bg-primaryBtn-hover h-[34px]"
+          >
+            Buy
+          </button>
         </div>
       </div>
     </div>
