71a95d7ea0a8ad1bf74db63d82c8f3f26bc1f7ee
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 14:27:47 +0100

    feat: add so that the cart number is only visible when the products in cart are over zero

--- frontend/src/components/pages/library/Header.tsx
+++ frontend/src/components/pages/library/Header.tsx
@@ -1,24 +1,29 @@
 import { useContext } from 'react';
 import cartLogo from '../../../assets/icons/cart.svg';
 import { LibraryContext } from '../../../context/LibraryContext';
+import { LoginContext } from '../../../context/LoginContext';
 
 const Header = () => {
   const libraryContext = useContext(LibraryContext);
+  const loginContext = useContext(LoginContext);
 
-  if (!libraryContext) {
+  if (!libraryContext || !loginContext) {
     return;
   }
 
   const { handleCartStateOnClick } = libraryContext;
+  const { cartOrders } = loginContext;
 
   return (
     <header className="bg-primaryBtn h-14 w-full flex items-center justify-between fixed top-0 z-20 pr-10 pl-10">
       <h1 className="font-bold text-2xl">QuirkReads</h1>
       <div className="flex gap-4">
         <div className="relative cursor-pointer">
-          <div className="rounded-full bg-slate-500 text-white absolute w-5 h-5 flex justify-center items-center">
-            <p>0</p>
-          </div>
+          {cartOrders.products.length > 0 && (
+            <div className="rounded-full bg-slate-500 text-white absolute w-5 h-5 flex justify-center items-center">
+              <p>{cartOrders.products.length}</p>
+            </div>
+          )}
           <img
             onClick={() => {
               handleCartStateOnClick(true);
