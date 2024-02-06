9c8466faae87cc354d6815f1d4406835e4dbbb44
RalfiSlask <nilssonnarki@gmail.com>
2024-02-06 06:28:54 +0100

    refactor main.tsx removed pages of create and login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginScreen from './components/pages/login/LoginScreen.tsx';
 import './styles/index.scss';
 import { LoginContextProvider } from './context/LoginContext.tsx';
 import { LibraryContextProvider } from './context/LibraryContext.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CreateAccountScreen from './components/pages/createaccount/CreateAccountScreen.tsx';
 import LibraryScreen from './components/pages/library/LibraryScreen.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -14,9 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <LibraryContextProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<LoginScreen />} />
-            <Route path="/create" element={<CreateAccountScreen />} />
-            <Route path="/library" element={<LibraryScreen />} />
+            <Route path="/" element={<LibraryScreen />} />
           </Routes>
         </BrowserRouter>
       </LibraryContextProvider>
