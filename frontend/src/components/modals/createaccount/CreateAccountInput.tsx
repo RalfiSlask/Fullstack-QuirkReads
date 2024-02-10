8505be328f64d036cee44934aee006bfdc51ae11
RalfiSlask <nilssonnarki@gmail.com>
2024-02-10 22:07:09 +0100

    feat: add more props for the validation of the input

--- frontend/src/components/modals/createaccount/CreateAccountInput.tsx
+++ frontend/src/components/modals/createaccount/CreateAccountInput.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { LoginContext } from '../../../context/LoginContext';
 import { ICreateAccountFormInputValues } from '../../../utils/types';
 import { getStringWithcapitalizedFirstLetter } from '../../../utils/helperfunctions';
@@ -7,25 +7,41 @@ type CreateAccountProps = {
   text: string;
   inputKey: keyof ICreateAccountFormInputValues;
   type: string;
+  formSubmitted: boolean;
+  setIsFormValid: React.Dispatch<
+    React.SetStateAction<{
+      email: boolean;
+      name: boolean;
+    }>
+  >;
+  setFormSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const CreateAccountInput: React.FC<CreateAccountProps> = ({ type, text, inputKey }) => {
+const CreateAccountInput: React.FC<CreateAccountProps> = ({
+  type,
+  text,
+  inputKey,
+  formSubmitted,
+  setIsFormValid,
+  setFormSubmitted,
+}) => {
+  const [errorMessage, setErrorMessage] = useState('');
   const loginContext = useContext(LoginContext);
 
   if (!loginContext) {
     return;
   }
 
-  const { createAccountInputValues, createAccountErrorMessage, handleCreateAccountInputOnChange } = loginContext;
+  const { createAccountInputValues, handleCreateAccountInputOnChange } = loginContext;
 
   return (
     <div className="flex flex-col gap-2 w-full">
       <div className="flex justify-between items-center">
         <label htmlFor={text}>{getStringWithcapitalizedFirstLetter(text)}</label>
-        <p className="text-red-500">{createAccountErrorMessage}</p>
+        <p className="text-red-500">{formSubmitted && errorMessage}</p>
       </div>
       <input
-        onInput={e => handleCreateAccountInputOnChange(inputKey, e)}
+        onInput={e => handleCreateAccountInputOnChange(inputKey, e, setErrorMessage, setIsFormValid, setFormSubmitted)}
         type={type}
         spellCheck="false"
         id={text}
